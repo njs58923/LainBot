575a8a48fc40fcb6f8f1612f571deb34d0547f64
njs58923 <njs58923@gmail.com>
2023-03-16 01:03:09 -0300

    multi interactions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const { Configuration, OpenAIApi } = require("openai");
 const readline = require("readline");
+const { build_prompt, build_messages, roles } = require("./resources/context");
 const fs = require("fs");
-const { stringify } = require("flatted");
+const { run_interactions } = require("./interactios");
 
 const configuration = new Configuration({
   apiKey: "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",
@@ -12,8 +13,6 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-const context = fs.readFileSync("./src/resources/contex.txt", "utf8");
 
 const getCircularReplacer = () => {
   const seen = new WeakSet();
@@ -28,33 +27,81 @@ const getCircularReplacer = () => {
   };
 };
 
-const generateResponse = async (prompt) => {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt,
-    temperature: 0.5,
-    max_tokens: 150,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0.6,
-    stop: ["\nSystem:", "\nAI:"],
-  });
+const chat_GPT3Turbo = async () => {
+  let messages = build_messages();
 
-  fs.writeFileSync("last_response.json", JSON.stringify(response, getCircularReplacer()));
+  const generateResponse = async (messages) => {
+    try {
+      var response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages,
+        temperature: 0.5,
+        max_tokens: 150,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0.6,
+        stop: [`\n${roles.system}:`, `\n${roles.ai}:`],
+      });
+    } catch (error) {
+      if (error.response.data) console.log(error.response.data);
+      else console.log(error);
+      throw error;
+    }
 
-  return response.data.choices[0].text.trim();
+    fs.writeFileSync("last_response.json", JSON.stringify(response, getCircularReplacer()));
+
+    return response.data.choices[0].message;
+  };
+
+  let input = { "#": { type: "client.resquest", message: 'fix the problem that I have in "C:\\MyAPP\\", the loop does not reach 100 for some reason' || (await getInput("You: ")) } };
+
+  messages.forEach((m) => console.log(m));
+
+  while (input !== "bye") {
+    const new_message = { role: roles.system, content: JSON.stringify(input) };
+
+    messages.push(new_message);
+    console.log(new_message);
+
+    let response = await generateResponse(messages);
+    response.content = response.content.slice(response.content.indexOf("{"));
+    messages.push(response);
+    console.log(response);
+    input = await run_interactions(response.content);
+  }
+  console.log("AI: Goodbye!");
+  process.exit(0);
 };
 
-let prompt = context;
-const chat = async () => {
-  let input = await getInput("You: ");
+const chat_davinci = async () => {
+  let prompt = build_prompt();
+
+  const generateResponse = async (prompt) => {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0.5,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: [`\n${roles.system}:`, `\n${roles.ai}:`],
+    });
+
+    fs.writeFileSync("last_response.json", JSON.stringify(response, getCircularReplacer()));
+
+    return response.data.choices[0].text.trim();
+  };
+
+  let input = { "#": { type: "client.resquest", message: 'fix the problem that I have in "C:\\MyAPP\\", the loop does not reach 100 for some reason' || (await getInput("You: ")) } };
+  console.log(input);
   while (input !== "bye") {
-    prompt += "\n" + `System: ${input}`;
-    console.log(prompt);
-    prompt += `\nAI: `;
+    const new_prompt = `${roles.system}: ` + JSON.stringify(input);
+    console.log(new_prompt);
+    prompt += "\n" + new_prompt + `\n${roles.ai}: `;
     let response = await generateResponse(prompt);
-    console.log(`AI: ${response}`);
-    input = await getInput("You: ");
+    console.log(`${roles.ai}: ${response}`);
+    input = await run_interactions(response);
   }
   console.log("AI: Goodbye!");
   process.exit(0);
@@ -68,4 +115,9 @@ const getInput = (prompt) => {
   });
 };
 
-chat();
+chat_GPT3Turbo();
+
+//{"read file": {"type":"files.readFileText", "path":"C:\\MyAPP\\Loop.cs"}}
+//{"#":{"type":"client.resquest","message":"arregla el problema que tengo en \"C:\\MyAPP\", el loop no llega hasta el 100 por algún motivo"}}
+
+//arregla el problema que tengo en "C:\MyAPP\", el loop no llega hasta el 100 por algún motivo
---
+++ src/resources/context.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+let context = fs.readFileSync("src\\resources\\context.txt", "utf8");
+const roles = { ai: "assistant", system: "user", context: "system" };
+exports.roles = roles;
+
+context = context.replace(/{{ai}}|{{system}}/g, (match) => {
+  if (match === "{{ai}}") {
+    return roles.ai; // Reemplazar '{{ai}}' con 'AI'
+  } else {
+    return roles.system; // Reemplazar '{{system}}' con 'System'
+  }
+});
+
+const start_messages = [
+  { role: roles.ai, content: `{"init": {"type":"test.init"}}` },
+  { role: roles.system, content: `{"init": "OK"}` },
+  { role: roles.ai, content: `{"type":"command.powershell", "commad": "Get-Date"}}` },
+  { role: roles.system, content: `{"get time": {"result":"17:56:45"}}` },
+  { role: roles.ai, content: `{"#": {"type":"test.wait"}}` },
+];
+
+exports.build_messages = () => {
+  return [{ role: roles.system, content: context }, ...start_messages];
+};
+
+exports.build_prompt = () => {
+  return `${context}\n\n${start_messages.map((m) => `${m.role}: ${m.content}`).join("\n")}`;
+};
